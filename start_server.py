575a5c7991aa127fb1763545fef0a401d5c72992
lewislucas <lewislucas@rocketmail.com>
2025-02-23 13:56:44 +0000

    setup done

---
+++ start_server.py
@@ -0,0 +1,26 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+
+hostname = "192.168.0.186"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header("Content-type", "text/html")
+		self.end_headers()
+		self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+		self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+		self.wfile.write(bytes("<body>", "utf-8"))
+		self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+		self.wfile.write(bytes("</body></html>", "utf-8"))
+webServer = HTTPServer((hostname, serverPort), MyServer)
+print("Server has started")
+try:
+	webServer.serve_forever()
+except KeyboardInterrupt:
+	pass
+
+webServer.server_close()
+print("Server stopped")
